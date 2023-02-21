3d66f8eb9e8db6e1f9a4da1530dc538a3552ece6
maxxxxxxy <cvb.cvb.2017.cvb@gmail.com>
2023-02-21 13:08:48 +0300

    modified controller

--- src/main/java/com/springex/springrest/entity/Function.java
+++ src/main/java/com/springex/springrest/entity/Function.java
@@ -17,45 +17,17 @@ import java.util.HashMap;
 @Table(name = "function")
 public class Function {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     Integer id;
-    String name;
+    String userName;
     Integer a;
     Integer b;
     Integer c;
-    public Function(String name,Integer a,Integer b,Integer c){
-        this.name = name;
+    public Function(String userName,Integer a,Integer b,Integer c){
+        this.userName = userName;
         this.a = a;
         this.b = b;
         this.c = c;
-    }
-
-
-    public  HashMap<String,String> quadro(Integer a,Integer b,Integer c){
-        HashMap<String,String> resultMap = new HashMap<>();
-        Integer discriminant = (int)Math.pow(b,2)-4*(a*c);
-        if(discriminant>0){
-            Double x1 =  (-b+Math.sqrt(discriminant)/2*a);
-            Double x2 =  (-b-Math.sqrt(discriminant)/2*a);
-            if(Math.pow(x1,2)*a+b*x1+c==0) {
-                resultMap.put("First root is: " + String.format("%.1f", x1), "Second root is: " + String.format("%.1f", x2));
-            }
-            else if(Math.pow(x2,2)*a+b*x2+c==0){
-                resultMap.put("First root is: " + String.format("%.1f", x1), "Second root is: " + String.format("%.1f", x2));
-            }
-            else  throw  new ArithmeticException("Not solved, cuz instance < 0");
-
-        }
-        else if(discriminant==0){
-            Double x = (double) (-b/2*a);
-            if(Math.pow(x,2)*a+b*x+c==0) {
-                resultMap.put("Only one root! ",String.format("%.1f", x));
-            }
-            else throw  new ArithmeticException("Not solved, cuz instance !=0");
-
-        }
-        else throw  new ArithmeticException("Not solved");
-        return resultMap;
     }
 }
 
