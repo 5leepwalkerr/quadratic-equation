7bfde171702f3e254a7c8efe609c49f9bfbeb5b6
maxxxxxxy <cvb.cvb.2017.cvb@gmail.com>
2023-04-24 19:11:26 +0300

    hotfix/add logger

--- src/main/java/com/springex/springrest/controller/FunctionController.java
+++ src/main/java/com/springex/springrest/controller/FunctionController.java
@@ -3,36 +3,30 @@ import com.springex.springrest.entity.Function;
 import com.springex.springrest.repository.FunctionRepository;
 import com.springex.springrest.service.FunctionService;
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
-import java.util.Map;
-import java.util.Objects;
 
 @Slf4j
 @RestController
 @RequestMapping("/quadro")
 public class FunctionController {
+    private static final Logger LOGGER = LoggerFactory.getLogger(FunctionController.class);
     @Autowired
     private FunctionService functionService;
     @Autowired
     private FunctionRepository functionRepository;
 
     @PostMapping("/function")
-    public ResponseEntity<?>saveFunction(@RequestBody Function function){
+    public ResponseEntity<?>saveFunction(@RequestBody Function function) {
         functionRepository.save(function);
-        HashMap<String,String> resultFunction = functionService.quadroFunction(function.getA(),function.getB(), function.getC());
-        return new ResponseEntity<>(resultFunction,HttpStatus.OK);
-    }
-    @PostMapping("/save")
-    public ResponseEntity<?>saveUser(@RequestBody Function function){
-      Map<String, Object> map = new HashMap<>();
-      map.put("status",1);
-      map.put("message","user in saved successfully");
-      functionRepository.save(function);
-      return new ResponseEntity<>(map,HttpStatus.CREATED);
+        HashMap<String, String> resultFunction = functionService.quadroFunction(function.getA(), function.getB(), function.getC());
+        LOGGER.info("Recieved POST request to '/function' with params: {}, {}, {}",function.getA(),function.getB(),function.getC());
+        return new ResponseEntity<>(resultFunction, HttpStatus.OK);
     }
 }
--- src/main/java/com/springex/springrest/service/Impl/FunctionServiceImp.java
+++ src/main/java/com/springex/springrest/service/Impl/FunctionServiceImp.java
@@ -1,37 +1,50 @@
 package com.springex.springrest.service.Impl;
 
 import com.springex.springrest.service.FunctionService;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.util.HashMap;
 
 @Service
+@Slf4j
 public class FunctionServiceImp implements FunctionService {
+    private static final Logger LOGGER = LoggerFactory.getLogger(FunctionServiceImp.class);
+
     @Override
     public HashMap<String, String> quadroFunction(Double a, Double b, Double c) {
         HashMap<String,String> resultMap = new HashMap<>();
         Double discriminant =(Math.pow(b,2))-(4*a*c);
-        if(discriminant>0){
-            Double x1 =  ((-b+Math.sqrt(discriminant))/(2*a)); //First root
-            Double x2 =  ((-b-Math.sqrt(discriminant))/(2*a)); // Second root
-            if(((a*(Math.pow(x1,2))+(b*x1))+c==0)&&((a*(Math.pow(x2,2)))+(b*x2)+c==0)) {
-                resultMap.put("First root is: " + String.format("%.2f", x1), "Second root is: " + String.format("%.2f", x2));
-            }
-            else if(a*(Math.pow(x1,2))+b*x1+c==0){
-                resultMap.put("Only first root in expression: "+String.format("%.2f",x1),"Second root is extraneous!");
-            }
-            else if(a*(Math.pow(x2,2))+b*x2+c==0){
-                resultMap.put("First root is extraneous!","Only second root in expression: "+String.format("%.2f",x2));
-            }
-            else  throw  new ArithmeticException("Not solved, cuz expression < 0");
 
+        if(discriminant>0){
+            Double root1 =  ((-b+Math.sqrt(discriminant))/(2*a)); //First root
+            Double root2 =  ((-b-Math.sqrt(discriminant))/(2*a)); // Second root
+            if(((a*(Math.pow(root1,2))+(b*root1))+c==0) && ((a*(Math.pow(root2,2)))+(b*root2)+c==0)) {
+                resultMap.put("First root is: " + String.format("%.2f", root1), "Second root is: " + String.format("%.2f", root2));
+                LOGGER.info("Expression solved!");
+            }
+            else if(a*(Math.pow(root1,2))+b*root1+c==0){
+                resultMap.put("Only first root in expression: "+String.format("%.2f",root1),"Second root is extraneous!");
+                LOGGER.info("Expression solved!");
+            }
+            else if(a*(Math.pow(root2,2))+b*root2+c==0){
+                resultMap.put("First root is extraneous!","Only second root in expression: "+String.format("%.2f",root2));
+                LOGGER.info("Expression solved!");
+            }
+            else{
+                LOGGER.error("Expression cannot be solved!");
+                throw  new ArithmeticException("Not solved, cuz expression < 0");
+            }
         }
         else if(discriminant==0){
-            Double x = (double) (-b/(2*a));
+            Double x = (-b/(2*a));
             resultMap.put("Only one root! ",String.format("%.3f", x));
 
         }
-        else throw  new ArithmeticException("Expression has no roots!, try another input params");
+        else throw new ArithmeticException("Expression has no roots!, try another input params");
+
         return resultMap;
     }
 }
